2699b22cd94f2df569ecd3516e6ca9c7f9e07237
tijothomas11 <tijothomas11@gmail.com>
2016-10-06 12:17:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,6 @@
 console.log('Loaded!');
 
 var img = document.getElementById('madi');
-var madi = { 
-  img.style.marginLeft= '100px';  
+img.onclick = function() {
+  img.style.marginLeft = '100px';  
 };
