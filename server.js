14388cf9aa328d102d98f78a1860042f47673d75
tijothomas11 <tijothomas11@gmail.com>
2016-10-06 16:57:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -85,11 +85,7 @@ app.get('/:articleName', function(req,res) {
     res.send(createTemplate(articles[articleName]));
 });
 
-var counter =0;
-app.get('/counter', function(req,res) {
-    counter=counter+1;
-    res.send(counter.toString());
-});
+
 
 
 app.get('/ui/style.css', function (req, res) {
