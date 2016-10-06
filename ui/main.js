8d4df7d3ea4ee400ca15d4bfc3c8bdda3ec3d2e0
tijothomas11 <tijothomas11@gmail.com>
2016-10-06 15:31:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,6 +5,9 @@ var marginLeft=0;
 function moveRight () {
     marginLeft = marginLeft +10;
     img.style.marginLeft = marginLeft + 'px';
+    if(marginLeft>800) {
+        marginLeft=0;
+    }
 }
 img.onclick = function() {
   var interval = setInterval(moveRight, 100);
