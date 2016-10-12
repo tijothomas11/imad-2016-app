b9dd92c818e39d390da90337bdfe1444c58fb2b0
tijothomas11 <tijothomas11@gmail.com>
2016-10-12 14:11:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -105,7 +105,7 @@ app.get('/ui/madi.png', function (req, res) {
 
 var names =[];
 app.get('/submit-mame/:name', function(req, res) {
-    var name = req.params.name;
+    var name = req.query.name;
     
     names.push(name);
     res.send(JSON.stringify(names));
