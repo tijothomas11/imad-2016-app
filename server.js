2a9ca19e877830b12cb4da5bed1b1842453e1654
tijothomas11 <tijothomas11@gmail.com>
2016-10-12 14:14:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -87,7 +87,7 @@ app.get('/counter', function(req,res) {
 });
 
 var names =[];
-app.get('/submit-mame', function(req, res) {
+app.get('/submit-name', function(req, res) {
     var name = req.query.name;
     
     names.push(name);
