abc8cb9c9ade6d2a971883850a1581c4e6c6f86d
tijothomas11 <tijothomas11@gmail.com>
2016-10-12 15:02:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -43,7 +43,7 @@ submit.onclick = function () {
     request.onreadystatechange = function () {
         if(request.readyState === XMLHttpRequest.DONE) {
             if(request.status === 200) {
-                var names = request.response.Text;
+                var names = request.responseText;
                 names = JSON.parse(names);
                 var list = '';
                 for (var i=0; i<names.length; i++) {
