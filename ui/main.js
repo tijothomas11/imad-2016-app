fb784e2b3238cde881964026912aa0e88a5cfea0
tijothomas11 <tijothomas11@gmail.com>
2016-10-12 12:54:28 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -30,6 +30,6 @@ button.onclick = function() {
         }
     };
     
-    request.open('GET', 'http://http://tijothomas11.imad.hasura-app.io', true);
+    request.open('GET', 'http://http://tijothomas11.imad.hasura-app.io/counter', true);
     request.send(null);
 };
