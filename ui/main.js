02ec767c920a077e0535fe18a49d7d2cafc86aa9
tijothomas11 <tijothomas11@gmail.com>
2016-10-12 14:51:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -35,8 +35,7 @@ button.onclick = function() {
 };
 
 //enter name
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
+
 var submit = document.getElementById('submit_btn');
 submit.onclick = function () {
     var request = new XMLHttpRequest();
@@ -56,7 +55,8 @@ submit.onclick = function () {
             }
         }
     };
-    
+    var nameInput = document.getElementById('name');
+    var name = nameInput.value;
     request.open('GET', 'http://tijothomas11.imad.hasura-app.io/submit-name?name=' + name, true);
     request.send(null);
 };
