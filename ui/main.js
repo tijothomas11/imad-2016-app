a4378fe185da3bf369ad1c00a45b7072d3ea1106
Your Name <you@example.com>
2016-10-27 16:18:08 +0530

    AV comments8

--- ui/main.js
+++ ui/main.js
@@ -18,20 +18,20 @@ var button = document.getElementById('counter');
 var counter=0;
 button.onclick = function() {
 
-    var request = new XMLHttpRequest();
+  var request = new XMLHttpRequest();
 
-    request.onreadystatechange = function () {
-        if(request.readyState === XMLHttpRequest.DONE) {
-            if(request.status === 200) {
-                var counter = request.responseText;
-                var span = document.getElementById('count');
-                span.innerHTML = counter.toString();
-            }
-        }
-    };
+  request.onreadystatechange = function () {
+    if(request.readyState === XMLHttpRequest.DONE) {
+      if(request.status === 200) {
+        var counter = request.responseText;
+        var span = document.getElementById('count');
+        span.innerHTML = counter.toString();
+      }
+    }
+  };
 
-    request.open('GET', 'http://tijothomas11.imad.hasura-app.io/counter', true);
-    request.send(null);
+  request.open('GET', 'http://tijothomas11.imad.hasura-app.io/counter', true);
+  request.send(null);
 };
 
 //enter name
