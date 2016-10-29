edb9e510d86db0f1b72da0ed4e1edc5793286b8f
Your Name <you@example.com>
2016-10-29 14:10:46 +0530

    time changing

--- ui/main.js
+++ ui/main.js
@@ -65,59 +65,58 @@ submit.onclick = function () {
 
 var submit1 = document.getElementById('comment_btn');
 submit1.onclick = function () {
-    var request1 = new XMLHttpRequest();
+  var request1 = new XMLHttpRequest();
 
-    request1.onreadystatechange = function () {
-        if(request1.readyState === XMLHttpRequest.DONE) {
-            if(request1.status === 200) {
-                var comments = request1.responseText;
-                comments = JSON.parse(comments);
-                var list = '';
-                for (var i=0; i<comments.length; i++) {
-                    list += '<li>' +comments[i]+ '</li>';
+  request1.onreadystatechange = function () {
+    if(request1.readyState === XMLHttpRequest.DONE) {
+      if(request1.status === 200) {
+        var comments = request1.responseText;
+        comments = JSON.parse(comments);
+        var list = '';
+        for (var i=0; i<comments.length; i++) {
+          list += '<li>' +comments[i]+ '</li>';
 
-                }
-                var ul = document.getElementById('commentlist');
-                ul.innerHTML = list;
-            }
         }
-    };
-    var commentInput = document.getElementById('comment');
-    var comment = commentInput.value;
-    request.open('GET', '/submit-comment?comment=' + comment, true);
-    request.send(null);
+        var ul = document.getElementById('commentlist');
+        ul.innerHTML = list;
+      }
+    }
+  };
+  var commentInput = document.getElementById('comment');
+  var comment = commentInput.value;
+  request.open('GET', '/submit-comment?comment=' + comment, true);
+  request.send(null);
 };
 
 //Time code
-function showDate()
-            {
-            	var now = new Date();
-            	var days = new Array('Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday');
-            	var months = new Array('January','February','March','April','May','June','July','August','September','October','November','December');
-            	var date = ((now.getDate()<10) ? "0" : "")+ now.getDate();
-            	function fourdigits(number)
-            	{
-            		return (number < 1000) ? number + 1900 : number;
-            	}
+function showDate() {
+	var now = new Date();
+	var days = new Array('Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday');
+	var months = new Array('January','February','March','April','May','June','July','August','September','October','November','December');
+	var date = ((now.getDate()<10) ? "0" : "")+ now.getDate();
+	function fourdigits(number)
+	{
+		return (number < 1000) ? number + 1900 : number;
+	}
 
-            	tnow=new Date();
-            	thour=now.getHours();
-            	tmin=now.getMinutes();
-            	tsec=now.getSeconds();
-            	var ap;
+	tnow=new Date();
+	thour=now.getHours();
+	tmin=now.getMinutes();
+	tsec=now.getSeconds();
+	var ap;
 
-            	if(thour==0){ap=" AM";thour=12;}
-                else if(thour<12){ap=" AM";}
-                else if(thour==12){ap=" PM";}
-                else if(thour>12){ap=" PM";thour-=12;}
+	if(thour==0){ap=" AM";thour=12;}
+    else if(thour<12){ap=" AM";}
+    else if(thour==12){ap=" PM";}
+    else if(thour>12){ap=" PM";thour-=12;}
 
 
-            	if (tmin<=9) { tmin="0"+tmin; }
-            	if (tsec<=9) { tsec="0"+tsec; }
+	if (tmin<=9) { tmin="0"+tmin; }
+	if (tsec<=9) { tsec="0"+tsec; }
 
-            	today = days[now.getDay()] + ", " + date + " " + months[now.getMonth()] + ", " + (fourdigits(now.getYear()));
-            	time = thour + ":" + tmin +":"+ tsec+""+ap;
-            	document.getElementById("dateDiv").innerHTML = today;
-            	document.getElementById("timeDiv").innerHTML = time;
-            }
-            setInterval("showDate()", 1000);
+	today = days[now.getDay()] + ", " + date + " " + months[now.getMonth()] + ", " + (fourdigits(now.getYear()));
+	time = thour + ":" + tmin +":"+ tsec+""+ap;
+	document.getElementById("dateDiv").innerHTML = today;
+	document.getElementById("timeDiv").innerHTML = time;
+}
+setInterval("showDate()", 1000);
