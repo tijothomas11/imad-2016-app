eeb6a53b27054124a67018d2e8b47833b999cd7b
Your Name <you@example.com>
2016-10-29 12:41:42 +0530

    name submit testing

--- ui/main.js
+++ ui/main.js
@@ -57,7 +57,7 @@ submit.onclick = function () {
     };
     var nameInput = document.getElementById('name');
     var name = nameInput.value;
-    request.open('GET', 'http://tijothomas11.imad.hasura-app.io/submit-name?name=' + name, true);
+    request.open('GET', '/submit-name?name=' + name, true);
     request.send(null);
 };
 
